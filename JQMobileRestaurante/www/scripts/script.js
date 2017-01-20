f137a4b41737ab8d0059f32f3dc4559ec8c29a97
DavoLarris <davo.larris@hotmail.com>
2017-01-20 09:13:45 +0100

    Finished

---
+++ JQMobileRestaurante/www/scripts/script.js
@@ -0,0 +1,19 @@
+﻿
+$(document).ready(function () {
+    $("#rateYo").rateYo({
+        starWidth: "25px",
+        rating: 3,
+        readOnly: true
+    });
+    $("#rateYo1").rateYo({
+        starWidth: "25px",
+        rating: 1.6,
+        readOnly: true
+    });
+    $("#rateYo2").rateYo({
+        starWidth: "25px",
+        rating: 4.4,
+        readOnly: true
+    });
+
+});
